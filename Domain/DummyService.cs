b29ac41ce70af87708ccaefaf0860c191fb0d4a6
aimenux <devplusdev@gmail.com>
2020-10-25 22:56:04 +0100

    Add project files.

---
+++ Domain/DummyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Domain
+{
+    public class DummyService : IDummyService
+    {
+        private static readonly Random Random = new Random(Guid.NewGuid().GetHashCode());
+
+        public string GetInfos()
+        {
+            const int length = 10;
+            const string chars = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var infos = Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)])
+                .ToArray();
+            return new string(infos);
+        }
+    }
+}
---
+++ WebAppUsingUnity/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Domain;
+using Microsoft.AspNet.WebFormsDependencyInjection.Unity;
+using Unity;
+
+namespace WebAppUsingUnity
+{
+    public class Global : HttpApplication
+    {
+        public void Application_Start(object sender, EventArgs e)
+        {
+            var container = this.AddUnity();
+            container.RegisterSingleton<IDummyService, DummyService>();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
